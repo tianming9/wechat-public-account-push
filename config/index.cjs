f270481ece86fe5b5c002563b314af38e65cd6a1
tianming9 <116866938+tianming9@users.noreply.github.com>
2022-10-28 14:55:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ocutv54u1XM-K3AX83v4rPt8mYnI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	93y34SUKsgK7pRCTWqrtc-NSI2rTCMcfxGlIih-WrLw',
+      useTemplateId: '93y34SUKsgK7pRCTWqrtc-NSI2rTCMcfxGlIih-WrLw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-14',
       festivals: [
