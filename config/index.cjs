7f3108c3a145fe25c031d17019bb9a9a86e1f45d
tianming9 <116866938+tianming9@users.noreply.github.com>
2022-11-01 09:21:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,24 +8,24 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx7f93bf9a4750a279',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'c0d494ca981183381de4f0f7f5499305',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '北京市',
+  CITY: '北京市',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小丫头',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '09-14',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
