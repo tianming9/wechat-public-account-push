ebac786db77543759e0d88c7bd793724d96fdf8e
tianming9 <116866938+tianming9@users.noreply.github.com>
2022-11-02 09:17:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ocutv50NtgQj0AFDy_MVUm0e7QO4',
+      id: 'ocutv51b5pW_EbeEg3LIlFbLHlDQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'fTXdIVpx3J4uQdFPswDEV0tz0papRnXTgWcmXB8esfM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
